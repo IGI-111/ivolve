1108bcc7eaed6d52663347fc861fbcabcb962d3b
IGI-111 <igi.222@gmail.com>
2014-09-12 15:54:30 +0200

    add basic program

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,11 @@
+#define MAGICKCORE_QUANTUM_DEPTH 16 // 16 bit color depth
+#define MAGICKCORE_HDRI_ENABLE 0    // no need for HDRI
+#include <Magick++/Image.h>
+
 int main(int argc, char *argv[])
 {
-	
+	using namespace Magick;
+	Image img("Law.jpg");
+	img.write("Law.gif");
 	return 0;
 }
