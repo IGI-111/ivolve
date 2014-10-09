04b18869383b415936625cc002a49a09c9d3975a
Jibril Saffi <igi.222@gmail.com>
2014-10-09 11:44:50 +0200

    add changes to premake4.lua

--- premake4.lua
+++ premake4.lua
@@ -1,9 +1,11 @@
 solution "Ivolve"
 	language "c++"
 	configurations { "Debug", "Release" }
-		buildoptions {"-std=c++14"}
+		buildoptions {"-std=c++11"}
 
 	project "Ivolve"
 		kind "WindowedApp"
-		links { "sfml-system", "sfml-window", "sfml-graphics" }
+		links { "sfml-system", "sfml-window", "sfml-graphics", "tgui" }
 		files { "src/*.cxx", "src/*.h" }
+		libdirs { "lib" }
+		includedirs { "include" }
